7581420bcab5ac9f8a45e7dbe20bc4e44db08095
Maria Rotolo <mariarotolo1994@gmail.com>
2021-11-23 19:41:36 +0100

    slider ok

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 function createProduct(parent, imgUrl, productTitle, textPrice) {
   const product = document.createElement("div");
   product.className = "product";
+  
 
   createImg(product, imgUrl, productTitle);
   createText(product, productTitle, textPrice);
@@ -27,12 +28,12 @@ function createText(parent, productTitle, textPrice) {
   parent.append(title, price);
 }
 
-fetch("https://fakestoreapi.com/products") // <== importare la lista prodotti in modo remoto
-  .then((response) => response.json())
-  .then((data) => {
-    products = data;
-    renderProducts();
-  }); 
+// fetch("https://fakestoreapi.com/products") // <== importare la lista prodotti in modo remoto
+  //.then((response) => response.json())
+  // .then((data) => {
+    //products = data;
+    //renderProducts();
+  //}); 
 
 
 const wrapperProducts = document.querySelector(".wrapper__products");
@@ -46,7 +47,28 @@ function renderProducts(listItems) {
 const getProductsList = async () => {
   const res = await fetch("https://fakestoreapi.com/products");
   const data = await res.json();
+  
   return renderProducts(data);
 
 };
 getProductsList();
+
+const images = [
+  "https://images.unsplash.com/photo-1493655161922-ef98929de9d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+  "https://images.unsplash.com/photo-1505022610485-0249ba5b3675?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80",
+  "https://images.unsplash.com/photo-1490481651871-ab68de25d43d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
+
+];
+
+const slider = document.querySelector(".overlay");
+
+let imagesIndex = 0;
+let changeImages = setInterval(() => {
+  slider.style.backgroundImage = `url(${images[imagesIndex]})`;
+
+  if( imagesIndex < images.length -1  ) {
+    imagesIndex++;
+  } else {
+    imagesIndex = 0;
+  }
+}, 2000);
